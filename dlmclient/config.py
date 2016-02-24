922fcf391019db0108604e48a75fa228defe58e3
Richard Guenther <richard.guenther@web.de>
2016-02-24 22:47:01 +0100

    config: Add error handling and logging to the 'get' and 'set' functions. Add ability to update every section in the conf file using a xml file

--- dlmclient/config.py
+++ dlmclient/config.py
@@ -17,18 +17,27 @@ class Config(object):
 		self.config.read(self.configfile)
 
 	def get(self, section, key):
-		self.config.get(section, key)
+		value = self.config.get(section, key)
+		if value is None:
+			logger.error('could not get option "%s" from section [%S]' %(key, section))
+		return value
 
 	def set(self, section, key, value):
-		self.config[section][key] = value
+		try:
+			ret = self.config.set(section, key, value)
+			logger.info('set "%s" in section [%s] to "%s"' %(key, section, value))
+		except Exception as err:
+			ret = 1
+			logger.error('could not set "%s" in section [%s] to "%s": %s' %(key, section, value, err))
+		return ret
 
-	def updateFromXml(self, xmlfile):
+	def updateFromXml(self, xmlfile, section='config'):
 		xml_conf = xml.readXmlFile(xmlfile)
-		if 'config' not in self.config.sections():
-			self.config['config'] = {}
+		if section not in self.config.sections():
+			self.config[section] = {}
 			
 		for key in xml_conf.keys():
-			self.set('config', key, str(xml_conf[key]))
+			self.set(section, key, str(xml_conf[key]))
 
 		with open(self.configfile, 'w') as configfile:
 			self.config.write(configfile)
